59375f734ea51d37883a7ac58243e0d79cb0955a
rajin-kichannagari <34991639+rajin-kichannagari@users.noreply.github.com>
2023-09-10 12:50:06 -0400

    Create advanced_sentiment_model.py

---
+++ models/advanced_sentiment_model.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+from utils.text_preprocessing import preprocess_text
+
+def analyze_sentiment(text):
+    sentiment_analyzer = pipeline("sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
+    preprocessed_text = preprocess_text(text)
+    result = sentiment_analyzer(preprocessed_text)
+    label = result[0]['label']
+    score = result[0]['score']
+    return label, score/Users/rajinkichannagari/Desktop/rajin's programs/Twitter-Sentiment-Analysis/utils/text_preprocessing.py
